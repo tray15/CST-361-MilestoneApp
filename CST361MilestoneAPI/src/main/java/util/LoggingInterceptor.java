17394a12d5d89014cb60e51bc260b7473fceed83
tray15 <tray15@my.gcu.edu>
2022-11-26 14:18:27 -0700

    update logger
    update logger to show when leaving method

--- CST361MilestoneAPI/src/main/java/util/LoggingInterceptor.java
+++ CST361MilestoneAPI/src/main/java/util/LoggingInterceptor.java
@@ -11,7 +11,16 @@ public class LoggingInterceptor implements Serializable {
 	
 	@AroundInvoke
 	public Object methodInterceptor(InvocationContext ctx) throws Exception {
+		Object callToMethod = null;
 		System.out.println("**************** Intercepting call to method: " + ctx.getTarget().getClass() + "." + ctx.getMethod().getName() + "()");
-		return ctx.proceed();
+		
+		try {
+			callToMethod = ctx.proceed();
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			System.out.println("**************** Leaving method: " + ctx.getTarget().getClass() + "." + ctx.getMethod().getName() + "()");
+		}
+		return callToMethod;
 	}
 }
